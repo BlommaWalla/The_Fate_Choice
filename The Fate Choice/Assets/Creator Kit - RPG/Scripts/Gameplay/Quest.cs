9ac6dacd8b08b7576d2eff59ae34b49af8176fb6
BlommaWalla <lutz.hahlbeck@gmail.com>
2020-11-12 14:37:20 +0100

    agöknaälgfn

--- The Fate Choice/Assets/Creator Kit - RPG/Scripts/Gameplay/Quest.cs
+++ The Fate Choice/Assets/Creator Kit - RPG/Scripts/Gameplay/Quest.cs
@@ -4,29 +4,25 @@ using RPGM.UI;
 using UnityEngine;
 using UnityEditor;
 using System;
+using UnityEngine.SceneManagement;
 
-namespace RPGM.Gameplay
-{
+namespace RPGM.Gameplay {
     /// <summary>
     /// This class implements quests.
     /// </summary>
-    public class Quest : MonoBehaviour
-    {
-        public enum SpawnMode
-        {
+    public class Quest : MonoBehaviour {
+        public enum SpawnMode {
             CloneAndEnable,
             CloneOnly
         }
 
         [System.Serializable]
-        public class ItemRequirement
-        {
+        public class ItemRequirement {
             public InventoryItem item;
             public int count = 1;
         }
 
-        public MonoScript script;
-
+        public bool battleAfterFinish;
 
         public string title;
         public string desc;
@@ -52,39 +48,39 @@ namespace RPGM.Gameplay
 
         GameModel model = Schedule.GetModel<GameModel>();
 
-        void Awake()
-        {
+        void Awake() {
             //if required, make sure that items that will be enabled by this quest are disabled
-            if (disableItemsOnStart)
-            {
+            if (disableItemsOnStart) {
                 if (enableOnQuestStart != null)
                     foreach (var i in enableOnQuestStart)
                         if (i != null)
                             i.SetActive(false);
 
-                switch (spawnMode)
-                {
+                switch (spawnMode) {
                     case SpawnMode.CloneAndEnable:
-                        foreach (var i in spawnOnQuestStart)
-                        {
+                        foreach (var i in spawnOnQuestStart) {
                             i.SetActive(false);
                         }
                         break;
                 }
             }
 
-           
+
 
         }
 
-        public void OnStartQuest()
-        {
+        public void Startbattle() {
+            SceneManager.LoadScene("SampleScene");
+
+
+        }
+
+        public void OnStartQuest() {
+            DontDestroyOnLoad(transform.gameObject);
             isFinished = false;
-            if (introCutscenePrefab != null)
-            {
+            if (introCutscenePrefab != null) {
                 var cs = Instantiate(introCutscenePrefab);
-                if (cs.audioClip != null)
-                {
+                if (cs.audioClip != null) {
                     cs.OnFinish += (i) => model.musicController.CrossFade(model.musicController.audioClip);
                 }
             }
@@ -92,11 +88,9 @@ namespace RPGM.Gameplay
                 foreach (var i in enableOnQuestStart)
                     if (i != null)
                         i.SetActive(true);
-            switch (spawnMode)
-            {
+            switch (spawnMode) {
                 case SpawnMode.CloneAndEnable:
-                    foreach (var i in spawnOnQuestStart)
-                    {
+                    foreach (var i in spawnOnQuestStart) {
                         var clone = GameObject.Instantiate(i);
                         clone.SetActive(true);
                         if (destroySpawnsOnQuestComplete) cleanup.Add(clone);
@@ -105,62 +99,51 @@ namespace RPGM.Gameplay
                     }
                     break;
                 case SpawnMode.CloneOnly:
-                    foreach (var i in spawnOnQuestStart)
-                    {
+                    foreach (var i in spawnOnQuestStart) {
                         var clone = GameObject.Instantiate(i);
                         if (destroySpawnsOnQuestComplete) cleanup.Add(clone);
                     }
                     break;
             }
 
-            
+
 
 
         }
 
-        public bool IsQuestComplete()
-        {
+        public bool IsQuestComplete() {
             var inv = new HashSet<string>(model.InventoryItems);
-            foreach (var i in requiredItems)
-            {
+            foreach (var i in requiredItems) {
                 if (inv.Contains(i.item.name) && model.GetInventoryCount(i.item.name) >= i.count) continue;
                 return false;
             }
             return true;
         }
 
-        public void RewardItemsToPlayer()
-        {
-            foreach (var i in rewardItems)
-            {
+        public void RewardItemsToPlayer() {
+            foreach (var i in rewardItems) {
                 MessageBar.Show($"You collected: {i.name} x {i.count}");
                 model.AddInventoryItem(i);
                 UserInterfaceAudio.OnCollect();
                 i.gameObject.SetActive(false);
             }
-            if (outroCutscenePrefab != null)
-            {
+            if (outroCutscenePrefab != null) {
                 var cs = Instantiate(outroCutscenePrefab);
-                if (cs.audioClip != null)
-                {
+                if (cs.audioClip != null) {
                     cs.OnFinish += (i) => model.musicController.CrossFade(model.musicController.audioClip);
                 }
             }
 
         }
 
-        public void OnFinishQuest()
-        {
-            if (destroySpawnsOnQuestComplete)
-            {
-                foreach (var i in cleanup)
-                {
+        public void OnFinishQuest() {
+            if (destroySpawnsOnQuestComplete) {
+                foreach (var i in cleanup) {
                     if (i != null) Destroy(i);
                 }
             }
 
-            foreach (var i in spawnOnQuestComplete)
-            {
+            foreach (var i in spawnOnQuestComplete) {
                 var clone = GameObject.Instantiate(i);
                 clone.SetActive(true);
             }
